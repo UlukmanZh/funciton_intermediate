1d8c09ce8045992470859eb72e87704713872b4d
Ulukman Zhamshitov <uakmatof@gmail.com>
2021-03-12 15:39:18 -0600

    changes in fuction

--- function_intermediate.py
+++ function_intermediate.py
@@ -2,24 +2,17 @@
 import random
 
 #print(randInt()) 		    # should print a random integer between 0 to 100
-def randInt(min=0   , max=0   ):
-    num = random.random()*100
-    return num
-
 
 #print(randInt(max=50)) 	    # should print a random integer between 0 to 50
-def randInt(min=0   , max=0   ):
-    num = random.random()*max
-    return num
-
-
 
 #print(randInt(min=50)) 	    # should print a random integer between 50 to 100
-def randInt(min=0   , max=0   ):
-    num = random.random()*50+min
-    return num
 
 #print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
-def randInt(min=0   , max=0   ):
+def randInt(min=0   , max=100   ):
     num = random.random()*(max-min)+min
-    return num
+    return round(num)
+
+print(randInt())
+print(randInt(max=50))
+print(randInt(min=50))
+print(randInt(min=50, max=500))
